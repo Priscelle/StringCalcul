4788a70d70d3f94badc721849a8834edbdb57919
Ella Priscelle <ellapriscelle@gmail.com>
2022-12-13 15:45:50 +0100

    First

---
+++ StringCalcul.Test/StringCalcul.cs
@@ -0,0 +1,10 @@
+namespace StringCalcul.Test;
+
+public class StringCalcul
+{
+    internal object Add(string numbers)
+    {
+        if (String.IsNullOrEmpty(numbers)) return 0;
+        return 1;
+    }
+}
---
+++ StringCalcul.Test/StringCalcul_Add.cs
@@ -0,0 +1,25 @@
+namespace StringCalcul.Test
+{
+    public class StringCalcul_Add
+
+    {
+        [Fact]
+        public void Returns0GivenEmptyString()
+        {
+            var calcul = new StringCalcul();
+            var result = calcul.Add("");
+
+            Assert.Equal(0, result);
+
+        }
+        [Fact]
+        public void Returns0GivenStringWith1()
+        {
+            var calcul = new StringCalcul();
+            var result = calcul.Add("1");
+
+            Assert.Equal(1, result);
+
+        }
+    }
+}
